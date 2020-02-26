2c272cc82556b70525eb7f1880ab7c36911d69e0
FimFi <55874743+FimFi@users.noreply.github.com>
2020-02-26 11:17:59 +0100

    Pusher nogle smarte classes

--- UTTT/src/uttt/BLL/field/Field.java
+++ UTTT/src/uttt/BLL/field/Field.java
@@ -5,10 +5,65 @@
  */
 package uttt.BLL.field;
 
+import java.util.List;
+import uttt.BLL.move.IMove;
+
 /**
  *
  * @author jigzi
  */
-public class Field {
+public class Field implements IField {
+
+    private String[][] macroBoard;
+    private String[][] board;
+    @Override
+    public void clearBoard() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<IMove> getAvailableMoves() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getPlayerId(int column, int row) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isFull() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Boolean isInActiveMicroboard(int x, int y) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String[][] getBoard() {
+        return board;
+    }
+
+    @Override
+    public String[][] getMacroboard() {
+        return macroBoard;
+    }
+
+    @Override
+    public void setBoard(String[][] board) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setMacroboard(String[][] macroboard) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
